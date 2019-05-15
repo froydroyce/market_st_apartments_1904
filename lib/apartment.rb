a3ad8f269c30101ec40fba5f81017c0da2de3533
Sejin Kim <sejinkim@Sejins-MacBook-Pro.local>
2019-05-15 09:20:55 -0600

    Initialize Apartment class

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,2 +1,9 @@
 class Apartment
+  def initialize(specs)
+    @number = specs[:name]
+    @monthly_rent = specs[:monthly_rent]
+    @bathrooms = specs[:bathrooms]
+    @bedrooms = specs[:bedrooms]
+    @renter = renter
+  end
 end
