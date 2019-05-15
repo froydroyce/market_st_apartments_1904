a7a25b7ad6cf2beb633423cb537a3cee6ebb5462
Sejin Kim <sejinkim@Sejins-MacBook-Pro.local>
2019-05-15 09:32:02 -0600

    Create add_renter method

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,9 +1,15 @@
 class Apartment
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
+
   def initialize(specs)
-    @number = specs[:name]
+    @number = specs[:number]
     @monthly_rent = specs[:monthly_rent]
     @bathrooms = specs[:bathrooms]
     @bedrooms = specs[:bedrooms]
     @renter = renter
+  end
+
+  def add_renter(name)
+    @renter = name
   end
 end
