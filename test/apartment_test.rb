ff6621d475c3e5556fb9d2b4883aeb3e0561aaf2
Sejin Kim <sejinkim@Sejins-MacBook-Pro.local>
2019-05-15 09:32:25 -0600

    Add tests for renter

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -7,10 +7,30 @@ require 'pry'
 class ApartmentTest < Minitest::Test
   def setup
     @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @jessie = Renter.new("Jessie")
   end
 
   def test_apartment_exists
 
     assert_instance_of Apartment, @a1
+  end
+
+  def test_apartment_has_specs
+
+    assert_equal "A1", @a1.number
+    assert_equal 1200, @a1.monthly_rent
+    assert_equal 1, @a1.bathrooms
+    assert_equal 1, @a1.bedrooms
+  end
+
+  def test_apartment_starts_with_no_renter
+
+    assert_nil @a1.renter
+  end
+
+  def test_apartment_can_add_renter
+    @a1.add_renter(@jessie)
+
+    assert_equal @jessie, @a1.renter
   end
 end
