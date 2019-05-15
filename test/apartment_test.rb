66076d01e96103874d146620c25bd47a1acc9b26
Sejin Kim <sejinkim@Sejins-MacBook-Pro.local>
2019-05-15 09:21:19 -0600

    Initialize Apartment test

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -6,5 +6,11 @@ require 'pry'
 
 class ApartmentTest < Minitest::Test
   def setup
-    
+    @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+  end
+
+  def test_apartment_exists
+
+    assert_instance_of Apartment, @a1
+  end
 end
