0f96a761cb3724323c82cd410373acdeb5f1deb7
Sejin Kim <sejinkim@Sejins-MacBook-Pro.local>
2019-05-15 10:30:32 -0600

    Finish building class

--- lib/building.rb
+++ lib/building.rb
@@ -1,5 +1,39 @@
 class Building
+  attr_reader :units
   def initialize
     @units = []
+  end
+
+  def add_unit(unit_num)
+    @units << unit_num
+  end
+
+  def average_rent
+    prices = []
+    @units.each do |unit|
+      prices << unit.monthly_rent
+    end
+    prices.sum.to_f / prices.length
+  end
+
+  def renter_with_highest_rent
+    units_with_renter = @units.find_all do |unit|
+      unit.renter != nil
+    end
+    unit_with_highest_rent = units_with_renter.max_by do |unit|
+      unit.monthly_rent
+    end
+    unit_with_highest_rent.renter
+  end
+
+  def annual_breakdown
+    annual = {}
+    units_with_renter = @units.find_all do |unit|
+      unit.renter != nil
+    end
+    units_with_renter.each do |unit|
+      annual[unit.renter.name] = unit.monthly_rent * 12
+    end
+    annual
   end
 end
